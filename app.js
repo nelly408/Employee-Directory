e0249468cb9affb0954b114d7d1aac0adb75f88d
nelly408 <alexrnelson>
2025-08-17 17:44:12 -0700

    resolved issues

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-// Write and export an Express app in app.js
-
-const express = require('express');
+import express from "express";
 const app = express();
-const employees = require('./employees.js')
-
-app.get("/", async(req, res)=>{
-        res.send("Hello Employees!");
-})
-
-app.get("/employees/:id", async(req, res)=>{
-        res.send(employees[employee]);
-})
-
-
 export default app;
+
+import employees from "#db/employees";
+
+app.route("/").get((req, res) => {
+  res.send("Hello employees!");
+});
+
+app.route("/employees").get((req, res) => {
+  res.send(employees);
+});
+
+// Note: this middleware has to come first! Otherwise, Express will treat
+// "random" as the argument to the `id` parameter of /employees/:id.
+app.route("/employees/random").get((req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+app.route("/employees/:id").get((req, res) => {
+  const { id } = req.params;
+
+  // req.params are always strings, so we need to convert `id` into a number
+  // before we can use it to find the employee
+  const employee = employees.find((e) => e.id === +id);
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
